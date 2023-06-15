d4dea479c5638492550f2f0b70f06a55ed0c0c87
Arsh <arshtandon121@gmail.com>
2023-06-14 20:49:00 -0700

    new section added services

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,6 +10,7 @@ import contactBackground from "../Assets/contactBackground.svg";
 import WhyUs from "../components/WhyUs";
 import'../CSS/home.css'
 import Navbar from "../components/Navbar";
+import Cards from "../components/Cards";
 
 export default function Home() {
   const TypedText = () => {
@@ -69,6 +70,10 @@ export default function Home() {
 
       <div id="services-section">
         <Services />
+      </div>
+ 
+      <div id="Cards-main">
+        <Cards/>
       </div>
 
       <div>
